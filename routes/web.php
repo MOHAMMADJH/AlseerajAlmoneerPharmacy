611da38cd61bb56007b527ad7183f4580ca2b390
mohammad hijji <mohjas97_fes@hotmail.com>
2018-11-18 12:43:39 +0200

    Save the new project to pharmacy

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+/*
+ * route::prefix('test')->groub(function (){
+    Route::get('/test1',function (){
+        return "مفيش إنترنت" ;
+    Route::get('/test2',function (){
+    return "2مفيش إنترنت" ;
+});
+*/
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
