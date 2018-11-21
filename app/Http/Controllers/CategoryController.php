138c25016a67bf4f34d82a105f64b858e7641e39
mohammad hijji <mohjas97_fes@hotmail.com>
2018-11-21 14:05:49 +0200

    add index F

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function  index()
+    {
+        $categories = Category::all();
+        return response()->json(['categories' => $categories]);
+    }
+
 }
