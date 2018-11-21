395e03242b7b8045febc91eb5158a575c2d4f6b5
mohammad hijji <mohjas97_fes@hotmail.com>
2018-11-21 13:52:12 +0200

    make migration for categories

--- database/migrations/2018_11_21_112745_create_categories_table.php
+++ database/migrations/2018_11_21_112745_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("name");
+            $table->string('image');
+            $table->mediumText('description');
             $table->timestamps();
         });
     }
