cfcfe67e9991f79b8144529e2a29cd0d4cdb697f
mohammad hijji <mohjas97_fes@hotmail.com>
2018-11-22 21:38:53 +0200

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,107 +5,39 @@ namespace App\Http\Controllers;
 use App\Category;
 use Illuminate\Http\Request;
 
-class CategoriesController extends Controller
+class CategoryController extends Controller
 {
-    //
-    public function index()
+    public function  index()
     {
         $categories = Category::all();
-//        $categories = Category::all(['name']);
-        return response()->json([
-            'status' => true,
-            'message' => 'Success',
-            'request' => 'Web Training course',
-            'categories' => $categories
-        ]);
+        return response()->json(['categories' => $categories]);
     }
-
-    public function show(Request $request)
-    {
-        $request->validate([
-            'id' => 'required|exists:categories,id|integer',
-        ]);
-
-        $category = Category::find($request->get('id'));
+public function create(Request $request)
+{
+    $request->validate([
+        'name' => 'required|string|min:3|max:20',
+        'description' => 'required|string|min:5|max:30',
+        'code' => 'required|integer'
+    ]) ;
+    $category = new Category();
+    $category->name = $request->get('name');
+    $category->description = $request->get('description');
+    $category->image = "";
+    $isSaved = $category->save();
+    if ($isSaved) {
         return response()->json([
             'status' => true,
+            'message' => 'Category Created Successfully',
             'category' => $category
         ]);
     }
-
-    public function create(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|min:3|max:20',
-            'description' => 'required|string|min:5|max:30',
-            'code' => 'required|integer'
-        ]);
-        $category = new Category();
-        $category->name = $request->get('name');
-        $category->description = $request->get('description');
-        $category->image = "";
-        $category->code = $request->get('code');
-        $isSaved = $category->save();
-        if ($isSaved) {
-            return response()->json([
-                'status' => true,
-                'message' => 'Category Created Successfully',
-                'category' => $category
-            ], 201);
-        } else {
+        else{
             return response()->json([
                 'status' => false,
-                'message' => 'Category Creation Failed'
-            ]);
+                'message' => 'Category Creation Failed']);
         }
     }
 
-    public function update(Request $request)
-    {
-        $request->validate([
-            'id' => 'required|exists:categories,id|integer',
-            'name' => 'required|string|min:3|max:20',
-            'description' => 'required|string|min:5|max:30',
-            'code' => 'required|integer'
-        ]);
-
-        $category = Category::find($request->get('id'));
-        $category->name = $request->get('name');
-        $category->description = $request->get('description');
-        $category->image = "";
-        $category->code = $request->get('code');
-        $isSaved = $category->save();
-        if ($isSaved) {
-            return response()->json([
-                'status' => true,
-                'message' => 'Category Updated Successfully',
-                'category' => $category
-            ]);
-        } else {
-            return response()->json([
-                'status' => false,
-                'message' => 'Category update Failed'
-            ]);
-        }
-    }
-
-    public function delete(Request $request)
-    {
-        $request->validate([
-            'id' => 'required|exists:categories,id|integer',
-        ]);
-
-        $isDeleted = Category::destroy($request->get('id'));
-        if ($isDeleted) {
-            return response()->json([
-                'status' => true,
-                'message' => 'Category deleted successfully'
-            ]);
-        } else {
-            return response()->json([
-                'status' => false,
-                'message' => 'Category delete Failed'
-            ]);
-        }
-    }
 }
+
+
